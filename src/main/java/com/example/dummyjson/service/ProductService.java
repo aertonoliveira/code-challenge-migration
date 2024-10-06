ca625bd0511691c8068102509bd6569073b8a453
Aerton Oliveira <aertonoliveiradosreis@gmail.com>
2024-10-06 13:15:05 -0300

    Adicionando o webclient e removendo o resttemplate e também removendo o base url do service

--- src/main/java/com/example/dummyjson/service/ProductService.java
+++ src/main/java/com/example/dummyjson/service/ProductService.java
@@ -1,30 +1,35 @@
 package com.example.dummyjson.service;
 
 import com.example.dummyjson.dto.Product;
+import com.example.dummyjson.dto.response.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    private final String BASE_URL = "https://dummyjson.com/products";
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     public List<Product> getAllProducts() {
-        Product[] products = restTemplate.getForObject(BASE_URL, Product[].class);
-        return Arrays.asList(products);
+        ProductResponse response = webClient.get()
+                .uri("/products")
+                .retrieve()
+                .bodyToMono(ProductResponse.class)  
+                .block();  
+
+        return response.getProducts();  
     }
 
+
     public Product getProductById(Long id) {
-        String url = BASE_URL + "/" + id;
-        return restTemplate.getForObject(url, Product.class);
+        return webClient.get()
+                .uri("/products/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .block();  
     }
 }
