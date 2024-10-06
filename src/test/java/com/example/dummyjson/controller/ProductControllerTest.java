7f8ba1b818ff734755706bf63377bace0f29ec60
Aerton Oliveira <aertonoliveiradosreis@gmail.com>
2024-10-06 13:16:34 -0300

    Retirando o Junit e colocando com SpringBootTest

--- src/test/java/com/example/dummyjson/controller/ProductControllerTest.java
+++ src/test/java/com/example/dummyjson/controller/ProductControllerTest.java
@@ -2,26 +2,24 @@ package com.example.dummyjson.controller;
 
 import com.example.dummyjson.dto.Product;
 import com.example.dummyjson.service.ProductService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class ProductControllerTest {
 
-    @InjectMocks
+    @Autowired
     private ProductController productController;
 
-    @Mock
+    @MockBean
     private ProductService productService;
 
     @Test
@@ -40,6 +38,7 @@ public class ProductControllerTest {
         List<Product> result = productController.getAllProducts();
         assertEquals(2, result.size());
         assertEquals("Product 1", result.get(0).getTitle());
+        assertEquals("Product 2", result.get(1).getTitle());
     }
 
     @Test
